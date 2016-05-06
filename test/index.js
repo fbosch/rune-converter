0aebe52266b2b75021ee93d558d85be78d476078
Frederik Bosch <frede.bosch@gmail.com>
2016-05-06 15:54:33 +0200

    created first unit tests

--- test/index.js
+++ test/index.js
@@ -1,8 +1,13 @@
 import assert from 'assert';
+import _ from 'lodash';
 import runeConverter from '../lib';
 
 describe('rune-converter', function () {
-  it('should have unit test!', function () {
-    assert(false, 'we expected this package author to add actual unit tests.');
-  });
+    it('should return the correct rune corrosponding to the given letter or return an empty string', function() {
+        assert(_.isEqual(runeConverter.getRune('a'),'ᚨ'), 'returns rune if it exists');
+        assert(_.isEqual(runeConverter.getRune('longstring'), ''), 'returns empty string if the input is more than a single letter');
+        assert(_.isEqual(runeConverter.getRune(1), ''), 'returns empty string if the input is a number');
+        assert(_.isEqual(runeConverter.getRune('å'),''), 'returns an empty string if there is no corrosponding rune to the given letter');
+    });
 });
+
